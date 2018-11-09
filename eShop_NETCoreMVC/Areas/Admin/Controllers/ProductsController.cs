5910308a6ca0a43445e4a516fed1c835c8f45224
Andrzej Pepłowski <pirx555@gmail.com>
2018-11-09 18:33:47 +0100

    Add controler Products. Add actions Products/Index and Products/Create (httpGet and httpPost). Add views Products/Index and Products/Create. Add partial view _TableButtonsEditDetailsDelete. Add fontawesome.

---
+++ eShop_NETCoreMVC/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop_NETCoreMVC.Data;
+using eShop_NETCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop_NETCoreMVC.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class ProductsController : Controller
+  {
+    private readonly ApplicationDbContext _db;
+
+    public ProductsController(ApplicationDbContext db)
+    {
+      _db = db;
+    }
+
+    public IActionResult Index()
+    {
+      return View(_db.Products.ToList());
+    }
+
+    //GET Create action method
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    //POST Create action method
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Product product)
+    {
+      if (ModelState.IsValid)
+      {
+        _db.Products.Add(product);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+
+      return View(product);
+    }
+  }
+}
